08c162037675c81f42a00552eee4610bf8b6c072
Jing Du <imkyito@gmail.com>
2020-07-08 12:27:46 -0600

    removed backend to start with zero

--- backend/src/route/api.v1.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userRoute from './v1/user.route';
-const prefix = '/api/v1';
-
-const router = express.Router();
-
-router.use(prefix, userRoute);
-
-export default router;
