a5d53d8d0f62fb3d301bac58e0ea9122ddd02544
Jing Du <imkyito@gmail.com>
2020-07-09 04:08:19 -0600

    pretty

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,19 +7,17 @@ import resolvers from './resolver';
 
 dotEnv.config();
 
-
-export const schema = makeExecutableSchema({
-        typeDefs: typeDefs,
-        resolvers: resolvers
-    }
-);
+const app = express();
+const port = process.env.SERVER_PORT;
+const schema = makeExecutableSchema({ typeDefs: typeDefs, resolvers: resolvers });
 const server = new ApolloServer({ schema });
 
-const app = express();
 server.applyMiddleware({ app: app, path: process.env.API_PREFIX });
-
-const port = process.env.SERVER_PORT;
 
 app.listen({ port }, () =>
     console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
 );
+
+
+
+
